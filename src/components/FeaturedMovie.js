50900358b16076509fb114cf17bf00c6306e3e1f
Luiz Felipe <lfelipecx@gmail.com>
2022-03-19 19:10:45 -0300

    corrigindo erro ortografico

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -26,7 +26,7 @@ export default ({item}) => {
                     <div className="featured--description">{item.overview}</div>
                     <div className="featured--buttons">
                         <a href={`/watch/${item.id}`} className="featured--watchbutton">► Assistir</a>
-                        <a href={`/list/add/${item.id}`}className="featured--mylistbutton">+ Minnha Lista</a>
+                        <a href={`/list/add/${item.id}`}className="featured--mylistbutton">+ Minha Lista</a>
                     </div>
                     <div className="featured--genres"><strong>Gêneros: </strong>{genres.join(', ')}</div>
               </div>
